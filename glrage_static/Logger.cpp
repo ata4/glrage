67fb7dd764df3ce42e60363a2fec2abbf1db531f
ata4 <barracuda415@yahoo.de>
2015-10-12 18:00:13 +0200

    Improved Logger class

--- glrage_static/Logger.cpp
+++ glrage_static/Logger.cpp
@@ -1,11 +1,30 @@
 #include "Logger.hpp"
 
 #include <string>
+#include <iostream>
 #include <psapi.h>
 
+bool Logger::m_verbose = false;
+LogTarget Logger::m_target = Win32Debug;
 TCHAR Logger::m_name[MAX_PATH];
 TCHAR Logger::m_info[1024];
 TCHAR Logger::m_line[1024];
+
+void Logger::setVerbose(bool verbose) {
+    m_verbose = verbose;
+}
+
+bool Logger::isVerbose() {
+    return m_verbose;
+}
+
+void Logger::setTarget(LogTarget target) {
+    m_target = target;
+}
+
+LogTarget Logger::getTarget() {
+    return m_target;
+}
 
 void Logger::log(void* returnAddress, const std::string& message) {
     log(returnAddress, message.c_str());
@@ -13,6 +32,15 @@ void Logger::log(void* returnAddress, const std::string& message) {
 
 void Logger::log(void* returnAddress, const char* message) {
     logImpl(returnAddress, message);
+}
+
+void Logger::logf(void* returnAddress, const std::string& message, ...) {
+    va_list list;
+    va_start(list, message);
+    vsnprintf_s(m_info, sizeof(m_info), _TRUNCATE, message.c_str(), list);
+    va_end(list);
+
+    logImpl(returnAddress, m_info);
 }
 
 void Logger::logf(void* returnAddress, const char* message, ...) {
@@ -25,7 +53,7 @@ void Logger::logf(void* returnAddress, const char* message, ...) {
 }
 
 void Logger::logImpl(void* returnAddress, const char* message) {
-    if (VERBOSE) {
+    if (m_verbose) {
         HMODULE handle;
         GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
             static_cast<LPCTSTR>(returnAddress), &handle);
@@ -36,5 +64,13 @@ void Logger::logImpl(void* returnAddress, const char* message) {
         _snprintf_s(m_line, sizeof(m_line), _TRUNCATE, "%s\n", message);
     }
 
-    OutputDebugString(m_line);
+    switch (m_target) {
+        case Console:
+            std::cout << m_line;
+            break;
+
+        case Win32Debug:
+            OutputDebugString(m_line);
+            break;
+    }
 }
--- glrage_static/Logger.hpp
+++ glrage_static/Logger.hpp
@@ -3,31 +3,43 @@
 #include <Windows.h>
 #include <intrin.h>
 #include <string>
+#include <cstdint>
 
 #ifdef DEBUG_LOG
-#define LOG(...) Logger::log(_ReturnAddress(), __VA_ARGS__)
+#define LOG(msg) Logger::log(_ReturnAddress(), msg)
 #define LOGF(...) Logger::logf(_ReturnAddress(), __VA_ARGS__)
 #else
-#define LOG(...)
+#define LOG(msg)
 #define LOGF(...)
 #endif
 
 #ifdef DEBUG_TRACE
-#define TRACE(...) Logger::log(_ReturnAddress(), __VA_ARGS__)
-#define TRACEF(...) Logger::logf(_ReturnAddress(), __VA_ARGS__)
+#define TRACE(msg) LOG(msg)
+#define TRACEF(...) LOGF(__VA_ARGS__)
 #else
-#define TRACE(...)
+#define TRACE(msg)
 #define TRACEF(...)
 #endif
 
+enum LogTarget {
+    Console,
+    Win32Debug
+};
+
 class Logger {
 public:
+    static void setVerbose(bool verbose);
+    static bool isVerbose();
+    static void setTarget(LogTarget target);
+    static LogTarget getTarget();
     static void log(void* returnAddress, const std::string& message);
     static void log(void* returnAddress, const char* message);
+    static void logf(void* returnAddress, const std::string& message, ...);
     static void logf(void* returnAddress, const char* message, ...);
 private:
     static void logImpl(void* returnAddress, const char*);
-    static const bool VERBOSE = true;
+    static bool m_verbose;
+    static LogTarget m_target;
     static TCHAR m_name[MAX_PATH];
     static TCHAR m_info[1024];
     static TCHAR m_line[1024];
