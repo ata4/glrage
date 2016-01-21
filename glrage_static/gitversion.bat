7fdb0c23000c38acee20240bc6d605e696676f4e
ata4 <barracuda415@yahoo.de>
2016-01-21 18:50:22 +0100

    Renamed git version script file and variable to be more reusable for other repos

--- glrage_static/gitversion.bat
+++ glrage_static/gitversion.bat
@@ -1,5 +1,5 @@
 @echo off
-set version_file=version.h
-set version_var=GLRAGE_VERSION
+set version_file=gitversion.h
+set version_var=GIT_VERSION
 for /f "tokens=*" %%i in ('git describe --tags') do set version_str=%%i
 echo #define %version_var% "%version_str%" > %version_file%
