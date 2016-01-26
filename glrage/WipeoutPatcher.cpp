5ad2330a1b2a34366073f2863de304d078d79d37
ata4 <barracuda415@yahoo.de>
2016-01-26 03:26:02 +0100

    Disabled alt-tab check that is broken in Windows 10
    Fixes #2

--- glrage/WipeoutPatcher.cpp
+++ glrage/WipeoutPatcher.cpp
@@ -14,18 +14,30 @@ bool WipeoutPatcher::applicable(const std::string& fileName) {
 }
 
 void WipeoutPatcher::apply() {
-    // This patch prevents Wipeout from tampering with the system settings, which
-    // are not always restored correctly, much to the annoyance of the user.
+    // Wipeout 95 apparently has a bug in the keyboard handling, which sometimes
+    // produces random crashes when a key is pressed for a prolonged period of time.
+    // The devs decided it would be a good idea to tamper with the system settings
+    // to reduce the key repeat rate to a minimum in order to "fix" this bug.
+    // The screensaver is also disabled for some unrelated reason.
+    // Unfortunately, these custom settings are not restored correctly when the
+    // game crashes or otherwise exits unexpectedly, much to the annoyance of the user.
+    // This patch replaces the SystemParametersInfo function with a stub to disable
+    // that behavior.
     m_tmp.clear();
     m_tmp << hookSystemParametersInfoA;
-    
     patch(0x7E0290, "30 6C 54 76", m_tmp);
 
-    // Disable title screen.
+    // Disable unskippable title screen, saves a few seconds of wait time.
     patch(0x46B885, "E8 B8 40 00 00", "90 90 90 90 90");
+
+    // Disable bugged alt+tab check that causes a segfault on Windows 10.
+    // GLRage doesn't really need it anyway and the only side effect is that the
+    // game continues to run in the background when tabbed.
+    patch(0x46AFB3, "00", "01");
 }
 
 BOOL WipeoutPatcher::hookSystemParametersInfoA(UINT uiAction, UINT uiParam, PVOID pvParam, UINT fWinIni) {
+    // do nothing
     return TRUE;
 }
 
